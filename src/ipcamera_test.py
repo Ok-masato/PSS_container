5c0746146973409796d3dd4fad97c44a5f0034c8
Ok-masato <okzaki0212@gmail.com>
2023-03-21 09:48:06 +0900

    ipcamera test

--- src/ipcamera_test.py
+++ src/ipcamera_test.py
@@ -1,44 +1,64 @@
-"""
-PythonのOpenCVを使用して、2つのIPカメラをRTSP経由で接続し、画面に表示します
-IPカメラのIPアドレス、ポート番号、ユーザー名、パスワードを適切に設定してください。
-"""
+import av
+import numpy as np
 import cv2
 
 # IPカメラ1の設定
-cam1_url = "rtsp://admin:123456@106.138.84.11:554/live/main"
-cam1 = cv2.VideoCapture(cam1_url)
+CAMERA_URL1 = "rtsp://admin:123456@114.151.100.188:554/live/jpeg/"
+# cam_url_1 = "rtsp://admin:123456@192.168.11.16/live/main/"
 
 # IPカメラ2の設定
-cam2_url = "rtsp://admin:123456@106.138.84.11:554/live/main"
-cam2 = cv2.VideoCapture(cam2_url)
+CAMERA_URL2 = "rtsp://admin:123456@114.151.100.188:555/live/jpeg/"
+# cam_url_2 = "rtsp://admin:123456@192.168.11.17/live/main/"
 
+# カメラ1とカメラ2のキャプチャーを開始する
+cap_1 = cv2.VideoCapture(cam_url_1)
+cap_2 = cv2.VideoCapture(cam_url_2)
+# cv2.CAP_DSHOW
+
+# フォーマット・解像度・FPSの設定
+#cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap_1.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap_1.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap_1.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap_1.set(cv2.CAP_PROP_FPS, 30)
+
+cap_2.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap_2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap_2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap_2.set(cv2.CAP_PROP_FPS, 30)
+
+# キャプチャーが正常に開始されたかどうかを確認する
+if not cap_1.isOpened() or not cap_2.isOpened():
+    print("Error opening video stream or file")
+
+# カメラ1とカメラ2のウィンドウを作成する
+cv2.namedWindow('Camera 1', cv2.WINDOW_NORMAL)
+cv2.namedWindow('Camera 2', cv2.WINDOW_NORMAL)
+
+# 並べて表示するためのウィンドウを作成する
+cv2.namedWindow('Cameras', cv2.WINDOW_NORMAL)
+
+# 2つのカメラから映像を取得し、1つの画面に並べて表示する
 while True:
-    # カメラ1の画像を取得
-    ret1, frame1 = cam1.read()
+    # カメラ1から1フレーム取得する
+    ret_1, frame_1 = cap_1.read()
+    # カメラ2から1フレーム取得する
+    ret_2, frame_2 = cap_2.read()
 
-    # カメラ2の画像を取得
-    ret2, frame2 = cam2.read()
+    # # 2つのフレームを横に並べる
+    frames = cv2.hconcat([frame_1, frame_2])
 
-    # 画像が正常に取得できた場合
-    if ret1 and ret2:
-        # 画像を表示
-        cv2.imshow('Camera 1', frame1)
-        cv2.imshow('Camera 2', frame2)
-    else:
-        print("Failed to open camera!")
-        exit()
+    # 画面に表示する
+    # cv2.imshow('Camera1', frame_1)
+    # cv2.imshow('Camera2', frame_2)
+    cv2.imshow('Cameras', frames)
 
 
     # キー入力を待つ
-    key = cv2.waitKey(1)
-
-    # 'q'キーが押された場合、プログラムを終了
-    if key == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# カメラを解放
-cam1.release()
-cam2.release()
-
-# 画面を閉じる
+# メモリを解放する
+cap_1.release()
+cap_2.release()
 cv2.destroyAllWindows()
